7f8e6d448bad1fab05f20741221e11f721708fb0
wahib.adiyatma <wahib.adiyatma@ui.ac.id>
2021-06-28 16:20:34 +0700

    setting up strapi for backend services

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd2a339d3dd84e85932cbb2d610663199'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fb8ff90c-4683-4b22-96f5-47406cf0248c'
+};
